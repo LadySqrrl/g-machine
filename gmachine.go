589e07f8ba14067272f22f274be96be4f69d2c62
LadySqrrl <72673087+LadySqrrl@users.noreply.github.com>
2021-08-16 15:48:38 -0500

    the start of things

---
+++ gmachine.go
@@ -0,0 +1,15 @@
+// Package gmachine implements a simple virtual CPU, known as the G-machine.
+package gmachine
+
+// DefaultMemSize is the number of 64-bit words of memory which will be
+// allocated to a new G-machine by default.
+const DefaultMemSize = 1024
+
+type GMachine struct {
+	P uint64
+	memory    []uint64
+}
+
+func New() *GMachine {
+	
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LadySqrrl/g-machine
+
+go 1.16
